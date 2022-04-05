76261c41cd070ab9eb63b8713766cc955156a0e5
lxx-lanfre <102993963+lxx-lanfre@users.noreply.github.com>
2022-04-05 19:05:50 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -11,7 +11,7 @@ exports.run = async (client, message, args) => {
   .setColor('#2667FF')
   .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
    .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Genel').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
-        .addField("» Linkler", ` [Davet Et]()` + "** | **" + `[Destek Sunucusu](https://discord.gg/2vPdmYz)`  + "** | **" + `[Oy Ver]()`  + "** | **" + `[Web Sitesi]()  `, false)
+        .addField("» Linkler", ` [Davet Et]()` + "** | **" + `[Destek Sunucusu]()`  + "** | **" + `[Oy Ver]()`  + "** | **" + `[Web Sitesi]()  `, false)
               return message.channel.send(Genel)
          
        
